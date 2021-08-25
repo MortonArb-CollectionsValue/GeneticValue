762db2e0b737061f1a406b476f00e4a3d6910b80
Sean <shoban@alumni.nd.edu>
2021-08-25 14:06:33 -0500

    looking at mismatches in ranking, depending on the buffer and eco or geo- added code

--- eco_geo_corr_ranking_IMLS.R
+++ eco_geo_corr_ranking_IMLS.R
@@ -3,8 +3,8 @@
 library(PerformanceAnalytics)
 
 #Set up paths
-data_dir <- "/Volumes/GoogleDrive/Shared drives/IMLS MFA/occurrence_points/outputs"
-output_dir <- "/Volumes/GoogleDrive/Shared drives/IMLS MFA/Genetic diversity value"
+data_dir <- "G:/Shared drives/IMLS MFA/occurrence_points/outputs"
+output_dir <- "G:/Shared drives/IMLS MFA/Genetic diversity value"
 
 #Read in file
 eco_geo_results<-read.csv(file.path(data_dir,"ExSituCoverage_BufferTable_6_30_21.csv"))
@@ -29,42 +29,48 @@ eco_geo_results[,2:(ncol(eco_geo_results)-2)]<-as.numeric(gsub("\\%.*","",as.mat
 #	CORRELATIONS	#
 #####################
 cols_eco_geo<-2:11
+pdf(file="eco_geo_corr_plots.pdf")
 #Trying to figure out if any of the measures are especially bad/ different
 #Look for correlations among geographic measures and identify when correlation is low, with raw and ranked values
 #This function calculates correlations, outputs top correlated metrics
 eco_geo_corr<-function(eg_matrix){
 	print("Correlation among <<measures>> themselves")
-	print(sort(rowSums(cor(eg_matrix[,cols_eco_geo],use="complete.obs")>.80),decreasing=T)[1:4])
-	print(sort(rowMeans(cor(eg_matrix[,cols_eco_geo],use="complete.obs")),decreasing=T)[1:4])
+	print(sort(rowSums(cor(eg_matrix[,cols_eco_geo],use="complete.obs")>.80),decreasing=T)[1:4])	#COUNT those corr w/ others above 0.80
+	print(sort(rowMeans(cor(eg_matrix[,cols_eco_geo],use="complete.obs")),decreasing=T)[1:4])		#just take MEAN corr w/ all others
 	print("Correlation among <<ranks>>- when put in order from measures")
 	print(sort(rowSums(cor(apply(eg_matrix[,cols_eco_geo],2,rank))>.80),decreasing=T)[1:4])
 	print(sort(rowMeans(cor(apply(eg_matrix[,cols_eco_geo],2,rank))),decreasing=T)[1:4])
 	chart.Correlation(eg_matrix[,cols_eco_geo], histogram = TRUE, method = "pearson")
+	rbind(rowSums(cor(eg_matrix[,cols_eco_geo],use="complete.obs")>.80),rowSums(cor(apply(eg_matrix[,cols_eco_geo],2,rank))>.80), rowMeans(cor(eg_matrix[,cols_eco_geo],use="complete.obs")), rowMeans(cor(apply(eg_matrix[,cols_eco_geo],2,rank))))
 	#To also add the one on ranking?
 }
-eco_geo_corr(eco_geo_results)
-	#Note: Geo10 and Eco10 are most different- not well correlated
-#Correlation among USA species
+#USA species
 eco_geo_results_usa<-eco_geo_results[-which(is.na(eco_geo_results[,9])),]
-eco_geo_corr(eco_geo_results_usa)
-#Correlation just for within rare and not rare sets
+#within rare and not rare sets
 eco_geo_results_LC<-eco_geo_results[eco_geo_results[,ncol(eco_geo_results)]=="LC",]
-eco_geo_corr(eco_geo_results_LC)
-	#Note: For geo10 and ecousa10, LC low correlation (probably too fine scale  for LC
 eco_geo_results_TH<-eco_geo_results[eco_geo_results[,ncol(eco_geo_results)]!="LC",]
-eco_geo_corr(eco_geo_results_TH)
-	#Note: here it is eco10 and eco50 low correlation (probably too fine scale  for LC
-#OVERALL: geo50, geo100, eco50, ecoUS50 have highest overall
 
-#But should probably find highest pairs and drop one of each pair... iteratively
-#This applies the function to subsets of: all, US, LC, Threatened
-lapply(list(eco_geo_results,eco_geo_results_usa,eco_geo_results_LC,eco_geo_results_TH),eco_geo_corr)
+#This applies the function to subsets of: 
+#				all, 			US, 				LC, 			Threatened
+#and saves as matrix with each row being one of those in the above function
+list_all_corr<-lapply(list(eco_geo_results,eco_geo_results_usa,eco_geo_results_LC,eco_geo_results_TH),eco_geo_corr)
+dev.off()
+#Over all species: geo10, eco10 and eco50 lower correlation But not bad... also epa ecoregions when looking at corr among ranks... because in ranking it will deal with the empty data in a funny way
+#US only: : geo10, eco10 and eco50 lower correlation But not bad- basically the same as all species, but corr EPA are higher prob because of the ecoregions
+#LC: geo10 especially low correlation , the rest are similar
+#TH: eco10, eco50 and to a lesser degree geo 10 and geo500 are low correlation
+#The best are typically geo50, geo100 and eco 100, sometimes eco50.  When the US EPA is good, the 50 and 100 are always 'better' than the 10
+
+#BUT should probably find highest pairs and drop one of each pair... iteratively
+
 
 
 #####################
 #	RANKING			#
 #####################
 
+#So, we have 10 different measures, some of which are highly correlated, some not. How to reconcile, choose among them for tanking
+#Could take the mean or majority decision...
 #One way to actually rank species is to identify those that most frequently are ranked in a given bunch, say in the top 10
 species_ranked1<-data.frame(sp_names_wcoll,rowSums(apply(eco_geo_results[,2:(ncol(eco_geo_results)-2)],2,rank)<10))
 #Another way to do actually rank species is to take the mean across rows in the rank order
@@ -74,20 +80,24 @@ colnames(species_ranked1)<-c("sp","rank"); colnames(species_ranked2)<-c("sp","ra
 #It really doesn't matter which approach :)
 cbind(species_ranked1[order(species_ranked1$rank),],species_ranked2[order(species_ranked2$rank,decreasing=T),])
 
-#Geo 10
-species_ranked_geo10<-data.frame(sp_names_wcoll,apply(eco_geo_results[,2:(ncol(eco_geo_results)-2)],2,rank)[,1])
+#Could look at those that might be most different and figure out why
+#Geo 50
+species_ranked_geo50<-data.frame(sp_names_wcoll,apply(eco_geo_results[,2:(ncol(eco_geo_results)-2)],2,rank)[,2])
 #Geo 100
-species_ranked_geo100<-data.frame(sp_names_wcoll,apply(eco_geo_results[,2:(ncol(eco_geo_results)-2)],2,rank)[,4])
-#Eco 10
-species_ranked_eco10<-data.frame(sp_names_wcoll,apply(eco_geo_results[,2:(ncol(eco_geo_results)-2)],2,rank)[,5])
+species_ranked_geo100<-data.frame(sp_names_wcoll,apply(eco_geo_results[,2:(ncol(eco_geo_results)-2)],2,rank)[,3])
+#Eco 50
+species_ranked_eco50<-data.frame(sp_names_wcoll,apply(eco_geo_results[,2:(ncol(eco_geo_results)-2)],2,rank)[,6])
 #Eco USA 50
-species_ranked_ecous50<-data.frame(sp_names_wcoll,apply(eco_geo_results[,2:(ncol(eco_geo_results)-2)],2,rank)[,10])
-colnames(species_ranked_geo10)<-c("sp","rank-geo10"); colnames(species_ranked_geo100)<-c("sp","rank-geo100")
-colnames(species_ranked_eco10)<-c("sp","rank-eco10"); colnames(species_ranked_ecous50)<-c("sp","rank-ecous50")
-cbind(species_ranked_geo10[order(species_ranked_geo10$rank),],species_ranked_geo100[order(species_ranked_geo100$rank),],
-	species_ranked_eco10[order(species_ranked_eco10$rank),],species_ranked_ecous50[order(species_ranked_ecous50$rank),])
+species_ranked_ecous50<-data.frame(sp_names_wcoll,apply(eco_geo_results[,2:(ncol(eco_geo_results)-2)],2,rank)[,9])
+colnames(species_ranked_geo50)<-c("sp","rank-geo50"); colnames(species_ranked_geo100)<-c("sp","rank-geo100")
+colnames(species_ranked_eco50)<-c("sp","rank-eco50"); colnames(species_ranked_ecous50)<-c("sp","rank-ecous50")
+cbind(species_ranked_geo50[order(species_ranked_geo50$rank),],species_ranked_geo100[order(species_ranked_geo100$rank),],
+	species_ranked_eco50[order(species_ranked_eco50$rank),],species_ranked_ecous50[order(species_ranked_ecous50$rank),])
 #Maybe 10 is just too fine scale!!
-
+ match(species_ranked_geo50[order(species_ranked_geo50$rank),1],species_ranked_geo100[order(species_ranked_geo100$rank),1])-1:41
+examine_changes<-data.frame(species=as.character(species_ranked_geo50[order(species_ranked_geo50$rank),1]),
+	change_eco50=(match(species_ranked_geo50[order(species_ranked_geo50$rank),1],species_ranked_eco50[order(species_ranked_eco50$rank),1])-1:41),
+	chage_ecoUS50=(match(species_ranked_geo50[order(species_ranked_geo50$rank),1],species_ranked_ecous50[order(species_ranked_ecous50$rank),1])-1:41))
 
 
 
